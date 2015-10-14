791e375233a9e919fe9aba8d6873e2767708745e
corysmc <cory@lucidchart.com>
2015-10-14 16:55:26 -0600

    Fixing path style js

--- components/0.0/components-min.js
+++ components/0.0/components-min.js
@@ -5866,7 +5866,7 @@ angular.module("lucidPathStyle", ['appConfig'])
             },
             replace: true,
             transclude: true,
-            templateUrl: config.componentsURL + 'components/0.0/path-style/lucid-path-style.html',
+            templateUrl: config.componentsURL + 'path-style/lucid-path-style.html',
 
             controller: function($scope) {
                 var lucidPath = new SVGMorpheus('#lucid-path-style');
