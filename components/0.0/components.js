a2a51cd8abe2e1774ca59bb9f63c0fab532260ec
corysmc <cory@lucidchart.com>
2015-10-14 16:53:15 -0600

    Update URL for github

--- components/0.0/components.js
+++ components/0.0/components.js
@@ -35,8 +35,8 @@ angular.module('appConfig', [])
 })
 
 .constant("config", {
-    'componentsURL': "/components/0.0/" //local dev
-        //'componentsURL': "http://lucidsoftware.github.io/particle/components/0.0/" //github
+    //'componentsURL': "/components/0.0/" //local dev
+    'componentsURL': "http://lucidsoftware.github.io/particle/components/0.0/" //github
 
 });
 
