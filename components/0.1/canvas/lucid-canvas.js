e5e94f12cb4bb5babb5f918f2667a551c7c15f6f
corysmc <cory@lucidchart.com>
2015-11-18 18:07:56 -0700

    Revert "updating canvas"
    This reverts commit fda138d8778710343012286823e657de82aad6ce.

--- components/0.1/canvas/lucid-canvas.js
+++ components/0.1/canvas/lucid-canvas.js
@@ -55,43 +55,43 @@ angular.module("lucidCanvas", ['appConfig'])
 
 
 
-                $scope.onDropComplete = function(data, event) {
-                    if (data && !$scope.manageShapes) {
-                        var index = $scope.blockElements.indexOf(data);
-                        //console.log('shape', index);
-                        if (index === -1) {
-                            var canvasX = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().left;
-                            var canvasY = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().top;
-                            //data.metrics = {};
-                            //console.log(data, event);
-                            data.metrics.x = event.x - canvasX - data.metrics.width / 2; //center block as it drops on canvas and expands
-                            data.metrics.y = event.y - canvasY - data.metrics.height / 2;
+                // $scope.onDropComplete = function(data, event) {
+                //     if (data && !$scope.manageShapes) {
+                //         var index = $scope.blockElements.indexOf(data);
+                //         //console.log('shape', index);
+                //         if (index === -1) {
+                //             var canvasX = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().left;
+                //             var canvasY = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().top;
+                //             //data.metrics = {};
+                //             //console.log(data, event);
+                //             data.metrics.x = event.x - canvasX - data.metrics.width / 2; //center block as it drops on canvas and expands
+                //             data.metrics.y = event.y - canvasY - data.metrics.height / 2;
 
-                            var newblock = JSON.parse(JSON.stringify(data));
-                            $scope.blockElements.push(newblock);
-                            //console.log('dropped on canvas', newblock, 'current array', $scope.blockElements);
-                        }
+                //             var newblock = JSON.parse(JSON.stringify(data));
+                //             $scope.blockElements.push(newblock);
+                //             //console.log('dropped on canvas', newblock, 'current array', $scope.blockElements);
+                //         }
 
-                    }
+                //     }
 
-                };
-                $scope.onDragSuccess = function(data, event) {
-                    if (event.x > 250) {
-                        var canvasX = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().left;
-                        var canvasY = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().top;
-                        //console.log('drag success', data, event);
-                        if (data) {
-                            data.metrics.x = event.x - canvasX - event.element.centerX;
-                            data.metrics.y = event.y - canvasY - event.element.centerY;
-                            data.shapepanel = false;
-                        }
-                    }
-                    // if (event.x < 220) {
-                    //     console.log('dropped in shapes'); //not sure how else to fix this.
-                    // }
-                    //console.log(data.metrics.x, data.metrics.y);
-                    //console.log('drag success');
-                };
+                // };
+                // $scope.onDragSuccess = function(data, event) {
+                //     if (event.x > 250) {
+                //         var canvasX = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().left;
+                //         var canvasY = angular.element(document.querySelector('#lucid-canvas'))[0].getBoundingClientRect().top;
+                //         //console.log('drag success', data, event);
+                //         if (data) {
+                //             data.metrics.x = event.x - canvasX - event.element.centerX;
+                //             data.metrics.y = event.y - canvasY - event.element.centerY;
+                //             data.shapepanel = false;
+                //         }
+                //     }
+                //     // if (event.x < 220) {
+                //     //     console.log('dropped in shapes'); //not sure how else to fix this.
+                //     // }
+                //     //console.log(data.metrics.x, data.metrics.y);
+                //     //console.log('drag success');
+                // };
             }
         };
     });
