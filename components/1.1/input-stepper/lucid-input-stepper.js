b0bdf10ffee4495445ef9dba424bedaf8247006a
corysmc <cory@lucidchart.com>
2015-11-30 12:42:50 -0700

    commit 1.1
    adding/changing buttcon directive, added label option, buttcon toggle,
    remove unnecessary styles, update components

---
+++ components/1.1/input-stepper/lucid-input-stepper.js
@@ -0,0 +1,69 @@
+angular.module('lucidInputStepper', ['appConfig'])
+    .directive('lucidInputStepper', function(config) {
+        return {
+            restrict: 'AE',
+            scope: {
+                unit: '@',
+                step: '@',
+                number: '=',
+                width: '@'
+            },
+            replace: true,
+            templateUrl: config.componentsURL + 'input-stepper/lucid-input-stepper.html',
+            controller: function($scope, $interval) {
+
+                $scope.stepperinput = $scope.number + $scope.unit;
+                $scope.removeText = function() {
+                    var text = $scope.stepperinput;
+                    var regex = /(\d+)/g;
+                    $scope.number = text.match(regex);
+                };
+
+                $scope.updateInput = function() {
+                    $scope.removeText();
+                    $scope.stepperinput = $scope.number + $scope.unit;
+                };
+                $scope.stepUp = function() {
+                    //first step up instantly on click
+
+                    $scope.removeText();
+                    $scope.number = parseInt($scope.number) + parseInt($scope.step);
+                    $scope.stepperinput = $scope.number + $scope.unit;
+                    //then continually step up if still holding.
+                    $promise = $interval(function() {
+                        $scope.removeText();
+                        $scope.number = parseInt($scope.number) + parseInt($scope.step);
+                        $scope.stepperinput = $scope.number + $scope.unit;
+
+                    }, 100);
+
+                };
+                $scope.stepDown = function() {
+
+                    //first step down instantly on click
+                    $scope.removeText();
+                    if($scope.number == 0){
+                        //console.log('zero');
+                        return;
+                    }
+                    $scope.number = parseInt($scope.number) + parseInt(-$scope.step);
+                    $scope.stepperinput = $scope.number + $scope.unit;
+                    //then continually step down if still holding.
+                    $promise = $interval(function() {
+
+                        $scope.removeText();
+                        if($scope.number == 0){
+                        //console.log('zero');
+                        return;
+                    }
+                        $scope.number = parseInt($scope.number) + parseInt(-$scope.step);
+                        $scope.stepperinput = $scope.number + $scope.unit;
+
+                    }, 100);
+                };
+                $scope.mouseUp = function() {
+                    $interval.cancel($promise);
+                };
+            }
+        };
+    });
