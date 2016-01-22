45aa5bfa857cbf741ab3186401282f40f07b8df2
corysmc <cory@lucidchart.com>
2016-01-22 09:57:53 -0700

    adding manager to docslist

--- prototype/press/docs/1.0/app.js
+++ prototype/press/docs/1.0/app.js
@@ -5,52 +5,48 @@ angular.module('particleApp', ['lucidComponents'])
     ////////////////
     ////START MAIN CTRL
     ////////////////
-    .controller('mainCtrl', function($scope, $rootScope, docTemplates) {
+    .controller('mainCtrl', function($scope, $rootScope, docTemplates, pressTemplates) {
 
-        $scope.pressTemplates = docTemplates;
+        $scope.pressStarters = docTemplates;
+        $scope.pressTemplates = pressTemplates;
         $scope.newDocument = true;
-        $scope.createDocument = function(category){
-            //var baseURL = 'http://corys-macbook-pro-2.local:5757/prototype/press/latest/#';
-            var baseURL = 'http://particle.golucid.co/prototype/press/latest/#';
-            window.location.href = baseURL + category.category;
-            console.log(category);
+        $scope.createDocument = function(group){
+            var baseURL = '/prototype/press/latest/#';
+            window.location.href = baseURL + group.groupname;
+            console.log(group);
         };
 
     })
     ///
     .factory('docTemplates', [function() {
         var docTemplates = [{
-            "category": "Blank",
+            "groupname": "Blank",
             "size": "Custom Size",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-placeholder-blank-page.png"
         }, {
-            "category": "Flyer",
+            "groupname": "Flyer",
             "size": "8.5 x 11 in",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-flyer-template-1.png"
         }, {
-            "category": "Brochure",
+            "groupname": "Brochure",
             "size": "11 x 8.5 in",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-brochure-template-1.png"
         }, {
-            "category": "Card",
+            "groupname": "Card",
             "size": "6 x 4 in",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-postcard-template-1.png"
         }, {
-            "category": "Poster",
+            "groupname": "Poster",
             "size": "18 x 24 in",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-poster-template-1.png"
         }, {
-            "category": "Report",
+            "groupname": "Report",
             "size": "8.5 x 11 in",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-annual-report-template-1.png"
         }, {
-            "category": "Business Card",
+            "groupname": "Business Card",
             "size": "3.5 x 2 in",
             "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-business-card-template-1.png"
-        }, {
-            "category": "Social Media",
-            "size": "3.667 x 8.5 in",
-            "url": "https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/press-leaflet-template-1.png"
         }];
         return docTemplates;
     }]);
