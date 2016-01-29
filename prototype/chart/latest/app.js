c060d67a527d6736730fa13ebe068344717a0c43
corysmc <cory@lucidchart.com>
2016-01-29 16:08:02 -0700

    adjusting shape groups, adding create custom group button back,

--- prototype/chart/latest/app.js
+++ prototype/chart/latest/app.js
@@ -527,6 +527,9 @@ angular.module('particleApp', ['lucidComponents', 'ngDraggable', 'ngSortable', '
             }
         };
         $scope.lucidGroups = [{
+            'groupname': 'Default',
+            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-flowchart.png'
+        }, {
             'groupname': 'Flowchart Shapes',
             'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-flowchart.png'
         }, {
@@ -557,13 +560,7 @@ angular.module('particleApp', ['lucidComponents', 'ngDraggable', 'ngSortable', '
             'groupname': 'Data Flow',
             'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-flowchart.png'
         }, {
-            'groupname': 'UI Mocks',
-            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-computer.png'
-        }, {
             'groupname': 'Org Charts',
-            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-flowchart.png'
-        }, {
-            'groupname': 'iOS 7 Mockups',
             'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-flowchart.png'
         }, {
             'groupname': 'Tables',
@@ -595,6 +592,21 @@ angular.module('particleApp', ['lucidComponents', 'ngDraggable', 'ngSortable', '
         }, {
             'groupname': 'Enterprise Integration',
             'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-flowchart.png'
+        }, {
+            'groupname': 'Equations',
+            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-computer.png'
+        }, {
+            'groupname': 'Floor Plans',
+            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-computer.png'
+        }, {
+            'groupname': 'Mind Mapping',
+            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-computer.png'
+        }, {
+            'groupname': 'Process Engineering',
+            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-computer.png'
+        }, {
+            'groupname': 'Venn Diagrams',
+            'image': 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/t-157/shapegroup-placeholder-computer.png'
         }];
         $scope.setGroup = function(groupname) {
             if (groupname === $scope.showGroup) { // is the tab already open?
@@ -899,7 +911,7 @@ angular.module('particleApp', ['lucidComponents', 'ngDraggable', 'ngSortable', '
                 "edit": true,
                 "shapes": [],
             };
-            $scope.customShapeGroups.push(newGroup);
+            $scope.customShapeGroups.splice(0, 0, newGroup);
             $rootScope.saveDocument();
         };
         $scope.editName = function(shapegroup, index) {
@@ -1072,120 +1084,120 @@ angular.module('particleApp', ['lucidComponents', 'ngDraggable', 'ngSortable', '
             }
         };
     })
-//this is used with shape libraries
-.animation('.shape-manager-drawer', ['$animateCss', function($animateCss) {
-    var lastId = 0;
-    var _cache = {};
+    //this is used with shape libraries
+    .animation('.shape-manager-drawer', ['$animateCss', function($animateCss) {
+        var lastId = 0;
+        var _cache = {};
 
-    function getId(el) {
-        var id = el[0].getAttribute("data-slide-toggle");
-        if (!id) {
-            id = ++lastId;
-            el[0].setAttribute("data-slide-toggle", id);
+        function getId(el) {
+            var id = el[0].getAttribute("data-slide-toggle");
+            if (!id) {
+                id = ++lastId;
+                el[0].setAttribute("data-slide-toggle", id);
+            }
+            return id;
         }
-        return id;
-    }
 
-    function getState(id) {
-        var state = _cache[id];
-        if (!state) {
-            state = {};
-            _cache[id] = state;
+        function getState(id) {
+            var state = _cache[id];
+            if (!state) {
+                state = {};
+                _cache[id] = state;
+            }
+            return state;
         }
-        return state;
-    }
 
-    function generateRunner(closing, state, animator, element, doneFn) {
-        return function() {
-            state.animating = true;
-            state.animator = animator;
-            state.doneFn = doneFn;
-            animator.start().finally(function() {
-                if (closing && state.doneFn === doneFn) {
-                    element[0].style.height = '';
-                }
-                // if (!closing && state.doneFn === doneFn) {
-                //     element[0].style.height = '';
-                // }
-                state.animating = false;
-                state.animator = undefined;
-                state.doneFn();
-            });
-        };
-    }
-
-    return {
-        addClass: function(element, className, doneFn) {
-            if (className === 'ng-hide') {
-                var state = getState(getId(element));
-                var height = (state.animating && state.height) ?
-                    state.height : element[0].offsetHeight;
-
-                var animator = $animateCss(element, {
-                    from: {
-                        height: height + 'px',
-                    },
-                    to: {
-                        height: '0px',
+        function generateRunner(closing, state, animator, element, doneFn) {
+            return function() {
+                state.animating = true;
+                state.animator = animator;
+                state.doneFn = doneFn;
+                animator.start().finally(function() {
+                    if (closing && state.doneFn === doneFn) {
+                        element[0].style.height = '';
                     }
+                    // if (!closing && state.doneFn === doneFn) {
+                    //     element[0].style.height = '';
+                    // }
+                    state.animating = false;
+                    state.animator = undefined;
+                    state.doneFn();
                 });
-                if (animator) {
-                    if (state.animating) {
-                        state.doneFn =
-                            generateRunner(true,
+            };
+        }
+
+        return {
+            addClass: function(element, className, doneFn) {
+                if (className === 'ng-hide') {
+                    var state = getState(getId(element));
+                    var height = (state.animating && state.height) ?
+                        state.height : element[0].offsetHeight;
+
+                    var animator = $animateCss(element, {
+                        from: {
+                            height: height + 'px',
+                        },
+                        to: {
+                            height: '0px',
+                        }
+                    });
+                    if (animator) {
+                        if (state.animating) {
+                            state.doneFn =
+                                generateRunner(true,
+                                    state,
+                                    animator,
+                                    element,
+                                    doneFn);
+                            return state.animator.end();
+                        } else {
+                            state.height = height;
+                            return generateRunner(true,
+                                state,
+                                animator,
+                                element,
+                                doneFn)();
+                        }
+                    }
+                }
+                doneFn();
+            },
+            removeClass: function(element, className, doneFn) {
+                if (className === 'ng-hide') {
+                    var state = getState(getId(element));
+                    var height = (state.animating && state.height) ?
+                        state.height : element[0].offsetHeight;
+
+                    var animator = $animateCss(element, {
+                        from: {
+                            height: '0px',
+                            opacity: 0
+                        },
+                        to: {
+                            height: height + 'px',
+                            opacity: 1
+                        }
+                    });
+
+                    if (animator) {
+                        if (state.animating) {
+                            state.doneFn = generateRunner(false,
                                 state,
                                 animator,
                                 element,
                                 doneFn);
-                        return state.animator.end();
-                    } else {
-                        state.height = height;
-                        return generateRunner(true,
-                            state,
-                            animator,
-                            element,
-                            doneFn)();
+                            return state.animator.end();
+                        } else {
+                            state.height = height;
+                            return generateRunner(false,
+                                state,
+                                animator,
+                                element,
+                                doneFn)();
+                        }
                     }
                 }
+                doneFn();
             }
-            doneFn();
-        },
-        removeClass: function(element, className, doneFn) {
-            if (className === 'ng-hide') {
-                var state = getState(getId(element));
-                var height = (state.animating && state.height) ?
-                    state.height : element[0].offsetHeight;
-
-                var animator = $animateCss(element, {
-                    from: {
-                        height: '0px',
-                        opacity: 0
-                    },
-                    to: {
-                        height: height + 'px',
-                        opacity: 1
-                    }
-                });
-
-                if (animator) {
-                    if (state.animating) {
-                        state.doneFn = generateRunner(false,
-                            state,
-                            animator,
-                            element,
-                            doneFn);
-                        return state.animator.end();
-                    } else {
-                        state.height = height;
-                        return generateRunner(false,
-                            state,
-                            animator,
-                            element,
-                            doneFn)();
-                    }
-                }
-            }
-            doneFn();
-        }
-    };
-}]);
+        };
+    }]);
