8d7bdfab8d390c148d5c7981ac3452683bf0aa00
corysmc <cory@lucidchart.com>
2016-10-24 16:30:23 -0600

    reorganizing data

--- js/component-data.js
+++ js/component-data.js
@@ -3,7 +3,7 @@ angular.module('lucidComponentFactory', [])
     .factory('lucidComponentFactory', function() {
         var service = [{
             //Basic Particle Components
-            "groupname": "Particles",
+            "groupname": "Editor Components",
             "groupid": "particles",
             "components": [{
                 "title": "Input",
@@ -13,35 +13,6 @@ angular.module('lucidComponentFactory', [])
                     "attr": "value",
                     "type": "text",
                     "details": "Initial value of input."
-                }, {
-                    "attr": "width",
-                    "type": "number",
-                    "details": "how wide the input should be (includes arrows)."
-                }, {
-                    "attr": "label",
-                    "type": "text",
-                    "details": "Optional: if you want a label below the input."
-                }, {
-                    "attr": "class",
-                    "type": "text",
-                    "details": "Optional: add 'condensed' for condensed aesthetic."
-                }]
-            }, {
-                "title": "Input Stepper",
-                "componentid": "input-stepper",
-                "description": "this component is used in the optionbar, the page color, etc.",
-                "inputs": [{
-                    "attr": "number",
-                    "type": "number",
-                    "details": "Initial value of input."
-                }, {
-                    "attr": "unit",
-                    "type": "text",
-                    "details": "Insert unit to be placed after number (px, in, pt)"
-                }, {
-                    "attr": "step",
-                    "type": "number",
-                    "details": "How much number is increased or decreased when clicking the up/down arrows"
                 }, {
                     "attr": "width",
                     "type": "number",
@@ -78,6 +49,35 @@ angular.module('lucidComponentFactory', [])
                 }]
 
             }, {
+                "title": "Input Stepper",
+                "componentid": "input-stepper",
+                "description": "this component is used in the optionbar, the page color, etc.",
+                "inputs": [{
+                    "attr": "number",
+                    "type": "number",
+                    "details": "Initial value of input."
+                }, {
+                    "attr": "unit",
+                    "type": "text",
+                    "details": "Insert unit to be placed after number (px, in, pt)"
+                }, {
+                    "attr": "step",
+                    "type": "number",
+                    "details": "How much number is increased or decreased when clicking the up/down arrows"
+                }, {
+                    "attr": "width",
+                    "type": "number",
+                    "details": "how wide the input should be (includes arrows)."
+                }, {
+                    "attr": "label",
+                    "type": "text",
+                    "details": "Optional: if you want a label below the input."
+                }, {
+                    "attr": "class",
+                    "type": "text",
+                    "details": "Optional: add 'condensed' for condensed aesthetic."
+                }]
+            }, {
                 "title": "Toggle",
                 "componentid": "toggle",
                 "description": "On/Off toggle state component used mostly in settings.",
@@ -107,6 +107,23 @@ angular.module('lucidComponentFactory', [])
                     "details": "Optional: add class of 'reverse-text' to make label text white and darken off state"
                 }]
             }, {
+                "title": "Edit in Place",
+                "componentid": "edit-in-place",
+                "description": "double click to edit in place",
+                "inputs": [{
+                    "attr": "ng-model",
+                    "type": "var",
+                    "details": "The text that you want to be editable"
+                }, {
+                    "attr": "placeholder",
+                    "type": "text",
+                    "details": "Optional: Add placeholder text if input is deleted"
+                }, {
+                    "attr": "edit",
+                    "type": "true/false",
+                    "details": "Optional: Edit mode can be toggled using outside variable. e.g. rename shape library from dropdown rather than double click."
+                }]
+            }, {
                 "title": "Buttcon",
                 "componentid": "buttcon",
                 "description": "Add a label to an icon, example is inside the colorpicker.",
@@ -128,17 +145,17 @@ angular.module('lucidComponentFactory', [])
                     "details": "Optional: add 'condensed' for condensed aesthetic. Note: should be 14px icon if condensed."
                 }]
             }, {
-                "title": "Buttons",
-                "componentid": "button",
-                "description": "Buttons can be created by adding a class",
+                "title": "Collapse Bar",
+                "componentid": "collapse-bar",
+                "description": "Used to separate data and settings",
                 "inputs": [{
-                    "attr": "color",
+                    "attr": "title",
                     "type": "text",
-                    "details": "colors available: blue, red, green, yellow, grey"
+                    "details": "Name the top bar"
                 }, {
-                    "attr": "icon",
-                    "type": "URL",
-                    "details": "insert URL of icon (should be white)."
+                    "attr": "closed",
+                    "type": "true/false",
+                    "details": "Optional: Set to true want the element closed by default"
                 }]
             }, {
                 "title": "Icon",
@@ -168,6 +185,19 @@ angular.module('lucidComponentFactory', [])
                 }],
                 "livelink": "http://www.lucidchart.com"
             }, {
+                "title": "Context Menu",
+                "componentid": "context-menu",
+                "description": "this component is used in the optionbar, the page color, etc.",
+                "inputs": [{
+                    "attr": "lucid-context-menu",
+                    "type": "",
+                    "details": "Include this on any element to add a right click context menu."
+                }, {
+                    "attr": "data-target",
+                    "type": "ID",
+                    "details": "Insert ID of context menu, that will show up when right clicked."
+                }]
+            }, {
                 "title": "More Drawer",
                 "componentid": "more-drawer",
                 "description": "this component is used in the optionbar, the page color, etc.",
@@ -176,15 +206,59 @@ angular.module('lucidComponentFactory', [])
                     "type": "number",
                     "details": "Insert the height the drawer should open."
                 }, {
-                    "attr": "options",
-                    "type": "Array",
-                    "details": "List out the options in an array. ['One', 'Two', 'Three']"
-                }, {
                     "attr": "tophandle",
                     "type": "true",
                     "details": "If true, drawer will open upwards. Only tested on a fixed bottom position."
                 }]
 
+            }]
+        }, {
+            //Basic Particle Components
+            "groupname": "Content Components",
+            "groupid": "components",
+            "components": [{
+                "title": "Buttons",
+                "componentid": "button",
+                "description": "Buttons can be created by adding a class",
+                "inputs": [{
+                    "attr": "color",
+                    "type": "text",
+                    "details": "colors available: blue, red, green, yellow, grey"
+                }, {
+                    "attr": "icon",
+                    "type": "URL",
+                    "details": "insert URL of icon (should be white)."
+                }]
+            }, {
+                "title": "Finger Tabs",
+                "componentid": "finger-tabs",
+                "description": "this component is used in modals and the user settings page",
+                "inputs": [{
+                    "attr": "name",
+                    "type": "text",
+                    "details": "Create a name for the finger tab."
+                }, {
+                    "attr": "icon",
+                    "type": "URL",
+                    "details": "Optional: Insert URL of icon. Coloring SVG is added through css"
+                }, {
+                    "attr": "selected-tab",
+                    "type": "variable",
+                    "details": "*Optional: link the current selected tab to another variable"
+                }],
+            }, {
+                "title": "Top Tabs",
+                "componentid": "top-tabs",
+                "description": "this component is used in press, the page color, etc.",
+                "inputs": [{
+                    "attr": "name",
+                    "type": "text",
+                    "details": "Create a name for the finger tab."
+                }, {
+                    "attr": "selected-tab",
+                    "type": "variable",
+                    "details": "*Optional: link the current selected tab to another variable"
+                }],
             }, {
                 "title": "Notifications",
                 "componentid": "notification",
@@ -200,36 +274,24 @@ angular.module('lucidComponentFactory', [])
                 }],
 
             }, {
-                "title": "Finger Tabs",
-                "componentid": "finger-tabs",
+                "title": "Progress Stepper",
+                "componentid": "progress-stepper",
                 "description": "this component is used in modals and the user settings page",
                 "inputs": [{
                     "attr": "name",
                     "type": "text",
                     "details": "Create a name for the finger tab."
                 }, {
-                    "attr": "icon",
-                    "type": "URL",
-                    "details": "Optional: Insert URL of icon. Coloring SVG is added through css"
-                },{
-                    "attr": "selected-tab",
+                    "attr": "selected-step",
                     "type": "variable",
-                    "details": "*Optional: link the current selected tab to another variable"
+                    "details": "*Optional: link the current selected step to another variable"
                 }],
-            }, {
-                "title": "Top Tabs",
-                "componentid": "top-tabs",
-                "description": "this component is used in press, the page color, etc.",
-                "inputs": [{
-                    "attr": "name",
-                    "type": "text",
-                    "details": "Create a name for the finger tab."
-                },{
-                    "attr": "selected-tab",
-                    "type": "variable",
-                    "details": "*Optional: link the current selected tab to another variable"
-                }],
-            }, {
+            }]
+        }, {
+            //Basic Particle Components
+            "groupname": "Modals",
+            "groupid": "modals",
+            "components": [{
                 "title": "Modal",
                 "componentid": "modal",
                 "description": "this component is used in the optionbar, the page color, etc.",
@@ -244,11 +306,11 @@ angular.module('lucidComponentFactory', [])
                 }, {
                     "attr": "width",
                     "type": "number",
-                    "details": "Width of modal."
+                    "details": "Width of modal content."
                 }, {
                     "attr": "height",
                     "type": "number",
-                    "details": "Height of modal"
+                    "details": "Height of modal content."
                 }, {
                     "attr": "confirm",
                     "type": "text",
@@ -263,70 +325,6 @@ angular.module('lucidComponentFactory', [])
                     "details": "Function called when user clicks confirm."
                 }],
 
-            }, {
-                "title": "Context Menu",
-                "componentid": "context-menu",
-                "description": "this component is used in the optionbar, the page color, etc.",
-                "inputs": [{
-                    "attr": "lucid-context-menu",
-                    "type": "",
-                    "details": "Include this on any element to add a right click context menu."
-                }, {
-                    "attr": "data-target",
-                    "type": "ID",
-                    "details": "Insert ID of context menu, that will show up when right clicked."
-                }]
-            }, {
-                "title": "Collapse Bar",
-                "componentid": "collapse-bar",
-                "description": "Used to separate data and settings",
-                "inputs": [{
-                    "attr": "title",
-                    "type": "text",
-                    "details": "Name the top bar"
-                }, {
-                    "attr": "closed",
-                    "type": "true/false",
-                    "details": "Optional: Set to true want the element closed by default"
-                }]
-            }, {
-                "title": "Edit in Place",
-                "componentid": "edit-in-place",
-                "description": "double click to edit in place",
-                "inputs": [{
-                    "attr": "ng-model",
-                    "type": "var",
-                    "details": "The text that you want to be editable"
-                }, {
-                    "attr": "placeholder",
-                    "type": "text",
-                    "details": "Optional: Add placeholder text if input is deleted"
-                }, {
-                    "attr": "edit",
-                    "type": "true/false",
-                    "details": "Optional: Edit mode can be toggled using outside variable. e.g. rename shape library from dropdown rather than double click."
-                }]
-            }, {
-                "title": "Onboarding Tip",
-                "componentid": "tip",
-                "description": "tips show up to show how to use the product, tips are quick introductions, to point out features",
-                "inputs": [{
-                    "attr": "showtip",
-                    "type": "var",
-                    "details": "when true, tip will show. If they have dismissed it, it will no longer show up."
-                }, {
-                    "attr": "dismiss",
-                    "type": "var",
-                    "details": "Set to true to dismiss tip. Once a user has dismissed the tip, it won't return"
-                }, {
-                    "attr": "caret",
-                    "type": "text",
-                    "details": "*Options: top, left, bottom, right. Adds appropriate caret and transition"
-                }, {
-                    "attr": "style",
-                    "type": "text",
-                    "details": "Set position with inline style or css."
-                }]
             }, {
                 "title": "Paywall",
                 "componentid": "paywall",
@@ -356,20 +354,33 @@ angular.module('lucidComponentFactory', [])
                     "type": "function",
                     "details": "*Optional: Function called when button is clicked."
                 }]
-            }, {
-                "title": "Progress Stepper",
-                "componentid": "progress-stepper",
-                "description": "this component is used in modals and the user settings page",
-                "inputs": [{
-                    "attr": "name",
-                    "type": "text",
-                    "details": "Create a name for the finger tab."
-                }, {
-                    "attr": "selected-step",
-                    "type": "variable",
-                    "details": "*Optional: link the current selected step to another variable"
-                }],
             }]
+        }, {
+            //Toolbar Components
+            "groupname": "User Education",
+            "groupid": "user-education",
+            "components": [{
+                "title": "Onboarding Tip",
+                "componentid": "tip",
+                "description": "tips show up to show how to use the product, tips are quick introductions, to point out features",
+                "inputs": [{
+                    "attr": "showtip",
+                    "type": "var",
+                    "details": "when true, tip will show. If they have dismissed it, it will no longer show up."
+                }, {
+                    "attr": "dismiss",
+                    "type": "var",
+                    "details": "Set to true to dismiss tip. Once a user has dismissed the tip, it won't return"
+                }, {
+                    "attr": "caret",
+                    "type": "text",
+                    "details": "*Options: top, left, bottom, right. Adds appropriate caret and transition"
+                }, {
+                    "attr": "style",
+                    "type": "text",
+                    "details": "Set position with inline style or css."
+                }]
+            }, ]
         }, {
             //Toolbar Components
             "groupname": "Toolbar Components",
